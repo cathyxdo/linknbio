7d33fe52ac0bad918b0647e8334624df9a5f2112
Cathy Do <catherinexdo@gmail.com>
2024-01-09 08:55:04 -0800

    adding permissions/cookie logic

--- backend/linklists/views.py
+++ backend/linklists/views.py
@@ -1,15 +1,22 @@
 from linklists.models import Link, SocialMedia, List
 from linklists.serializers import LinkSerializer, SocialMediaSerializer, ListSerializer
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 
 class ListList(generics.ListCreateAPIView):
     queryset = List.objects.all()
     serializer_class = ListSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return List.objects.filter(user=user)
 
 class ListDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = List.objects.all()
     serializer_class = ListSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class CreateLink(generics.ListCreateAPIView):
     queryset = Link.objects.all()
