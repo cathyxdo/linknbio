99d5315dbd0d8e9fd87c57c5940fbac7f672702a
Cathy Do <catherinexdo@gmail.com>
2024-03-05 11:18:02 -0800

    added style

--- frontend/app/dashboard/appearance/page.tsx
+++ frontend/app/dashboard/appearance/page.tsx
@@ -1,7 +1,18 @@
-export default function Page() {
+
+import Appearance from "@/components/Appearance";
+import { ListProfile } from "@/shared/interfaces";
+
+export default async function Page() {
+    const list = await getList();
+
     return (
-        <div>
-            hello
-        </div>
+        <Appearance data={list} />
     )
 }
+
+async function getList(): Promise<ListProfile> {
+    const res = await fetch('http://127.0.0.1:8000/api/lists/1', {cache: "no-store"});
+    const list: ListProfile = await res.json();
+    console.log(list);
+    return list;
+}; 
