fb938ed93c975d70c1649a13569a8ce908b1ebe0
Cathy Do <catherinexdo@gmail.com>
2024-04-01 16:18:01 -0700

    add color fields

--- frontend/shared/interfaces.ts
+++ frontend/shared/interfaces.ts
@@ -10,7 +10,9 @@ export interface ListProfile {
     background_color: string;
     background_image: string;
     link_bubble_style: string;
+    link_bubble_color: string;
     link_font: string;
+    link_font_color: string;
     social_media_icons_location: string;
     social_media_profiles: SocialMediaProfile[];
     links: Link[];
