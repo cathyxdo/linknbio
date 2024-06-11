e5a8e16b0abb4cca4bf5ce0a56b19db08fb46b27
Cathy Do <catherinexdo@gmail.com>
2024-06-11 12:49:17 -0700

    removing old user stuff, adding jwt

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -16,13 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import GoogleLogin, CustomLoginView
+#from .views import GoogleLogin, CustomLoginView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', CustomLoginView.as_view(), name='custom_login'),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('dj-rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
+    #path('login/', CustomLoginView.as_view(), name='custom_login'),
+    #path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    #path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    #path('dj-rest-auth/google/', GoogleLogin.as_view(), name='google_login'),
     path('', include('linklists.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
