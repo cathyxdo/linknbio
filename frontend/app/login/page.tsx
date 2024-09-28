c705f520977f4814b4ef0ef1df766e148ab9fc0e
Cathy Do <catherinexdo@gmail.com>
2024-09-28 08:09:45 -0700

    remove unnecessary import

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,9 +1,7 @@
-import LoginForm from "@/components/LoginForm";
 import Login from "@/components/Login";
 export default function Page() {
     return (
         <>
-            {/* //<LoginForm /> */}
             <Login />
         </>
     )
--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,9 +1,7 @@
-import SignUpForm from "@/components/SignUpForm";
 import SignUp from "@/components/SignUp";
 export default function Page() {
     return (
         <>
-            {/**<SignUpForm /> **/}
             <SignUp />
         </>
     )
