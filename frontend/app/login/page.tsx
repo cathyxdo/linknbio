e4f10b90b79a1abd7d68b334919b48e85f53ed1a
Cathy Do <catherinexdo@gmail.com>
2024-10-10 20:38:33 -0700

    add title to page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,4 +1,9 @@
 import Login from "@/components/Login";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Login | Linknbio',
+};
 export default function Page() {
     return (
         <>
--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,4 +1,10 @@
 import SignUp from "@/components/SignUp";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Signup | Linknbio',
+};
+
 export default function Page() {
     return (
         <>
