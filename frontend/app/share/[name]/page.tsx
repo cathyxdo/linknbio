859bbd769738b6fef4b7f74d1f532acb777db540
Cathy Do <catherinexdo@gmail.com>
2024-11-14 11:49:32 -0800

    change page to use components and call api to log page views

--- frontend/app/share/[name]/page.tsx
+++ frontend/app/share/[name]/page.tsx
@@ -1,89 +1,16 @@
-import InstagramIcon from "@mui/icons-material/Instagram";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import YouTubeIcon from "@mui/icons-material/YouTube";
-import TwitterIcon from "@mui/icons-material/Twitter";
 import Image from "next/image";
 import { ListProfile } from "@/shared/interfaces";
-
+import Link from "@/components/share/Link";
+import SocialMedia from "@/components/share/SocialMedia";
+import { notFound } from "next/navigation";
 export default async function Page({ params }: any) {
   const list = await getList(params.name);
-  const imageStyle = {
-    borderRadius: "50%",
-  };
 
-  const renderSocialMediaIcons = (type: string) => {
-    switch (type) {
-      case "instagram":
-        return (
-          <InstagramIcon
-            className="ease-in transform hover:scale-110 transition duration-150"
-            style={{ color: list.profile_font_color }}
-          />
-        );
-      case "facebook":
-        return (
-          <FacebookIcon
-            className="ease-in transform hover:scale-110 transition duration-150"
-            style={{ color: list.profile_font_color }}
-          />
-        );
-      case "youtube":
-        return (
-          <YouTubeIcon
-            className="ease-in transform hover:scale-110 transition duration-150"
-            style={{ color: list.profile_font_color }}
-          />
-        );
-      case "twitter":
-        return (
-          <TwitterIcon
-            className="ease-in transform hover:scale-110 transition duration-150"
-            style={{ color: list.profile_font_color }}
-          />
-        );
-      default:
-        return null;
-    }
-  };
+  if (!list) {
+    notFound();
+  }
+  await logPageView(list.id);
 
-  function getLinkBubbleColorStyle() {
-    if (list.link_bubble_style.includes("bubble_outline")) {
-      return {
-        outlineStyle: "solid",
-        outlineColor: list.link_bubble_color,
-        color: list.link_font_color,
-      };
-    } else {
-      return {
-        backgroundColor: list.link_bubble_color,
-        color: list.link_font_color,
-      };
-    }
-  }
-  function getLinkBubbleStyle() {
-    switch (list.link_bubble_style) {
-      case "bubble_filled":
-        return "";
-      case "bubble_filled_rounded":
-        return "rounded-xl";
-      case "bubble_filled_circular":
-        return "rounded-full";
-      case "bubble_outline":
-        return "";
-      case "bubble_outline_rounded":
-        return "rounded-xl";
-      case "bubble_outline_circular":
-        return "rounded-full";
-      case "bubble_shadow":
-        return "shadow-lg";
-      case "bubble_shadow_rounded":
-        return "shadow-lg rounded-xl";
-      case "bubble_shadow_circular":
-        return "shadow-lg rounded-full";
-      default:
-        return "border-2 border-black";
-    }
-  }
   return (
     <div
       className={`flex flex-col items-center h-screen font-${list.profile_font}`}
@@ -97,7 +24,7 @@ export default async function Page({ params }: any) {
                 src={list.profile_photo_url}
                 width={100}
                 height={100}
-                style={imageStyle}
+                style={{borderRadius: "50%"}}
                 alt="profile-photo"
                 className=""
               />
@@ -115,54 +42,49 @@ export default async function Page({ params }: any) {
         </div>
         {list.social_media_icons_location === "top" && (
           <div>
-            <ul className="flex gap-4 mt-4 mb-4">
+            <div className="flex gap-4 mt-4 mb-4">
               {list.social_media_profiles &&
                 list.social_media_profiles.map((profile) => (
-                  <li key={profile.id}>
-                    <a target="_blank" href={profile.link}>
-                      {renderSocialMediaIcons(profile.type)}
-                    </a>
-                  </li>
+                  <SocialMedia 
+                    key={profile.id} 
+                    profileLink={profile.link}
+                    profileType={profile.type}
+                    profileFontColor={list.profile_font_color}
+                    profileId={profile.id}
+                  />
                 ))}
-            </ul>
+            </div>
           </div>
         )}
         <div className="flex flex-col gap-6 w-full">
           {list.links &&
             list.links.map((link) => (
-              <a
-                target="_blank"
-                key={link.id}
-                href={link.link}
-                style={getLinkBubbleColorStyle()}
-                className={`${getLinkBubbleStyle()} px-4 py-4  ease-in transform hover:scale-105 transition duration-150`}
-              >
-                {link.link_photo_url && (
-                  <Image
-                    src={link.link_photo_url} // Fallback to a default image if photo is not available
-                    width={45}
-                    height={45}
-                    style={imageStyle}
-                    alt={link.title}
-                    className="absolute left-1 top-1"
-                  />
-                )}
-                <p className="text-sm">{link.title}</p>
-              </a>
+              <Link 
+                key = {link.id}
+                linkBubbleColor={list.link_bubble_color} 
+                linkFontColor={list.link_font_color} 
+                linkBubbleStyle={list.link_bubble_style}
+                linkId={link.id}
+                linkLink={link.link}
+                linkTitle={link.title}
+                linkPhotoUrl={link.link_photo_url}
+              />
             ))}
         </div>
         {list.social_media_icons_location === "bottom" && (
           <div>
-            <ul className="flex gap-4 mt-4 mb-4">
+            <div className="flex gap-4 mt-4 mb-4">
               {list.social_media_profiles &&
                 list.social_media_profiles.map((profile) => (
-                  <li key={profile.id}>
-                    <a target="_blank" href={profile.link}>
-                      {renderSocialMediaIcons(profile.type)}
-                    </a>
-                  </li>
+                  <SocialMedia 
+                    key={profile.id} 
+                    profileLink={profile.link}
+                    profileType={profile.type}
+                    profileFontColor={list.profile_font_color}
+                    profileId={profile.id}
+                  />
                 ))}
-            </ul>
+            </div>
           </div>
         )}
         <ul></ul>
@@ -171,12 +93,31 @@ export default async function Page({ params }: any) {
   );
 }
 
-async function getList(name: string): Promise<ListProfile> {
+async function getList(name: string): Promise<ListProfile | null> {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL_SERVER}/api/lists/view/` + name,
     { cache: "no-store" }
   );
-  const list: ListProfile = await res.json();
 
+  if (!res.ok) {
+    return null
+  }
+
+  const list: ListProfile = await res.json();
+  console.log(res.ok)
   return list;
+}
+
+async function logPageView(listId: number) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL_SERVER}/api/log/page_view/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ list_id: listId }),
+  });
+
+  if (!res.ok) {
+    console.error("Failed to log page view");
+  }
 }
