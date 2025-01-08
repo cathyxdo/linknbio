5ebf83d3b2d4e93b94cdf55658931e7f3e42f1dd
Cathy Do <catherinexdo@gmail.com>
2025-01-07 16:33:11 -0800

    add delete profile image button

--- frontend/components/ProfileImageModal.tsx
+++ frontend/components/ProfileImageModal.tsx
@@ -17,14 +17,15 @@ export default function ProfileImageModal({
           <div className="absolute inset-0 bg-gray-500 opacity-50"></div>
         </div>
         <div className="relative bg-white rounded-lg p-8 ">
-          <h3>Upload Profile Picture</h3>
+          <h3 className="text-lg font-semibold mb-6">Upload Profile Picture</h3>
           <ProfileImageCropper
             id={id}
             closeProfileImageModal={closeProfileImageModal}
             handleProfileImageUpdate={handleProfileImageUpdate}
           />
+          <button className="mt-6 text-sm bg-red-400 rounded-lg hover:bg-red-600 px-4 py-2 font-semibold text-white">Delete Profile Picture</button>
           <button
-            className="absolute top-0 right-0 m-4 text-gray-500 hover:text-gray-700 hover:bg-stone-100 p-2 rounded-full"
+            className="absolute top-0 right-0 m-4 text-gray-500 hover:text-gray-700 hover:bg-stone-200 p-2 rounded-full"
             onClick={closeProfileImageModal}
           >
             <svg
