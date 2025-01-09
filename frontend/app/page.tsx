2859f8b7023a7970c2f70a0059c7d6005ec14a10
Cathy Do <catherinexdo@gmail.com>
2025-01-08 19:59:27 -0800

    add feature content and modified padding, text styling

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -43,17 +43,15 @@ export default function Home() {
         </nav>
       </header>
       <div className="w-full flex flex-col items-center">
-        <div className="container flex flex-col md:flex-row gap-12 items-center pt-20 md:py-64 pb-10 md:px-10 px-4">
-          <div className=" flex flex-col">
-            <h1 className="text-5xl">
-              Centralize your online presence in one place using{" "}
-              <span className="font-bold">Linknbio</span>
+        <div className="container flex flex-col md:flex-row gap-12 items-center pt-20 md:py-48 pb-10 md:px-20 px-4">
+          <div className=" flex flex-col flex-1">
+            <h1 className="text-5xl font-extrabold tracking-tight">
+                Centralize your online presence in one place using Linknbio
             </h1>
-            <h3 className="mt-4">
-              Gather all your links, social media, music, videos, and more in one
+            <h3 className="mt-6 text-xl">
+            Gather all your links, social media, music, videos, and more in one
               place. Create a page that matches your style and never worry about
-              changing your bio link again.
-            </h3>
+              changing your bio link again.            </h3>
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded-full mt-8"
               onClick={() => handleAuthButtonClick("signup")}
@@ -61,7 +59,34 @@ export default function Home() {
               Get Started
             </button>
           </div>
-          <div className="">
+          <div className="flex-1">
+            <Image
+              src="/hero-graphic.png"
+              width={1178}
+              height={1276}
+              alt="hero-image"
+              className="w-full h-auto "
+            />
+          </div>
+        </div>
+        <div className="bg-indigo-200 py-20 md:py-40 px-4 md:px-24 w-full flex flex-col md:flex-row-reverse items-center gap-12">
+          <div className="flex flex-col flex-1">
+            <h1 className="text-5xl font-extrabold text-blue-900 tracking-tight">
+              Your page up and running in minutes, customized to your personal brand
+            </h1>
+            <h3 className="mt-4 font-semibold text-blue-900">
+              No need to download anything. Easily add all your links and personalize the page to your style.
+            </h3>
+            <div>
+              <button
+                className="bg-blue-900 hover:bg-blue-950 text-white font-bold py-4 px-8 rounded-full mt-6"
+                onClick={() => handleAuthButtonClick("signup")}
+              >
+                Get Started
+              </button>
+              </div>
+          </div>
+          <div className="flex-1">
             <Image
               src="/hero-graphic.png"
               width={1178}
@@ -71,27 +96,25 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="bg-indigo-300 py-40 px-4 md:px-10 w-full flex flex-col md:flex-row-reverse items-center gap-12">
-          <div className="flex flex-col">
-            <h1 className="text-3xl">
-                Centralize your online presence in one place using{" "}
-                <span className="font-bold">Linknbio</span>
+        <div className="bg-fuchsia-200 py-20 md:py-40 px-4 md:px-24 w-full flex flex-col md:flex-row items-center gap-12">
+          <div className="flex flex-col flex-1">
+            <h1 className="text-5xl font-extrabold text-rose-900 tracking-tight">
+            Share your Linknbio on your Instagram, Youtube, TikTok, Twitter and other platforms
             </h1>
-            <h3 className="mt-4">
-              Gather all your links, social media, music, videos, and more in one
-              place. Create a page that matches your style and never worry about
-              changing your bio link again.
+            <h3 className="mt-4 font-semibold text-rose-900">
+            Add your Linknbio URL wherever your audience is. Direct all your following to one location.
+
             </h3>
             <div>
               <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-full mt-6"
+                className="bg-rose-900 hover:bg-rose-950 text-white font-bold py-4 px-8 rounded-full mt-6"
                 onClick={() => handleAuthButtonClick("signup")}
               >
                 Get Started
               </button>
               </div>
           </div>
-          <div className="">
+          <div className="flex-1">
             <Image
               src="/hero-graphic.png"
               width={1178}
@@ -101,57 +124,25 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="bg-fuchsia-200 py-40 px-4 md:px-10 w-full flex flex-col md:flex-row items-center gap-12">
-          <div className="flex flex-col">
-            <h1 className="text-3xl">
-                Centralize your online presence in one place using{" "}
-                <span className="font-bold">Linknbio</span>
+        <div className="bg-rose-100 py-20 md:py-40 px-4 md:px-24 w-full flex flex-col md:flex-row-reverse items-center gap-12">
+          <div className="flex flex-col flex-1">
+            <h1 className="text-5xl font-extrabold text-indigo-600 tracking-tight">
+              Analyze the performance of your Linknbio
+
             </h1>
-            <h3 className="mt-4">
-              Gather all your links, social media, music, videos, and more in one
-              place. Create a page that matches your style and never worry about
-              changing your bio link again.
+            <h3 className="mt-4 text-indigo-600 font-semibold">
+              See what's working and what's not. Track your page performance over time and keep your audience engaged.
             </h3>
             <div>
               <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-full mt-6"
+                className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-4 px-8 rounded-full mt-6"
                 onClick={() => handleAuthButtonClick("signup")}
               >
                 Get Started
               </button>
               </div>
           </div>
-          <div className="">
-            <Image
-              src="/hero-graphic.png"
-              width={1178}
-              height={1276}
-              alt="hero-image"
-              className="w-full h-auto"
-            />
-          </div>
-        </div>
-        <div className="bg-rose-400 py-40 px-4 md:px-10 w-full flex flex-col md:flex-row-reverse items-center gap-12">
-          <div className="flex flex-col">
-            <h1 className="text-3xl">
-                Centralize your online presence in one place using{" "}
-                <span className="font-bold">Linknbio</span>
-            </h1>
-            <h3 className="mt-4">
-              Gather all your links, social media, music, videos, and more in one
-              place. Create a page that matches your style and never worry about
-              changing your bio link again.
-            </h3>
-            <div>
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-full mt-6"
-                onClick={() => handleAuthButtonClick("signup")}
-              >
-                Get Started
-              </button>
-              </div>
-          </div>
-          <div className="">
+          <div className="flex-1">
             <Image
               src="/hero-graphic.png"
               width={1178}
