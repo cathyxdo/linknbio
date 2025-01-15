9d51811fac13aaaf0f8997825953ac0cb156f66f
Cathy Do <catherinexdo@gmail.com>
2025-01-15 08:38:34 -0800

    remove sticky css setting on nav bar

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -29,8 +29,8 @@ export default function DashboardLayout({
   };
 
   return (
-    <section className="bg-stone-100">
-      <nav className="flex justify-center top-0 z-50 ">
+    <section className="bg-stone-100 min-h-screen">
+      <nav className="flex fixed w-full justify-center top-0 left-0 z-50 ">
         <div
           className="block fixed md:hidden p-4 cursor-pointer bg-white w-full border-b"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
