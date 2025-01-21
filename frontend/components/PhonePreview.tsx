07a1328e87a92f899db1d6bf4ca634fe090187bd
Cathy Do <catherinexdo@gmail.com>
2025-01-21 09:53:25 -0800

    modify so that on mobile, the phone preview takes up the whole screen

--- frontend/components/PhonePreview.tsx
+++ frontend/components/PhonePreview.tsx
@@ -16,28 +16,28 @@ export default function PhonePreview({ pageData }: PhonePreviewProps) {
       case "instagram":
         return (
           <InstagramIcon
-            className="text-m"
+            className="ease-in transform hover:scale-110 text-4xl transition duration-150 md:text-xl"
             style={{ color: pageData.profile_font_color }}
           />
         );
       case "facebook":
         return (
           <FacebookIcon
-            className="text-m"
+            className="ease-in transform hover:scale-110 text-4xl transition duration-150 md:text-xl"
             style={{ color: pageData.profile_font_color }}
           />
         );
       case "youtube":
         return (
           <YouTubeIcon
-            className="text-m"
+            className="ease-in transform hover:scale-110 text-4xl transition duration-150 md:text-xl"
             style={{ color: pageData.profile_font_color }}
           />
         );
       case "twitter":
         return (
           <TwitterIcon
-            className="text-m"
+            className="ease-in transform hover:scale-110 text-4xl transition duration-150 md:text-xl"
             style={{ color: pageData.profile_font_color }}
           />
         );
@@ -87,35 +87,35 @@ export default function PhonePreview({ pageData }: PhonePreviewProps) {
   return (
     <div
       style={{ backgroundColor: pageData.background_color }}
-      className={`fixed  h-[692px] w-[320px] px-2 py-4 mb-4 space-y-2  border-gray-900 border-8 rounded-3xl shadow-sm flex flex-col items-center text-center font-${pageData.profile_font}`}
+      className={`fixed overflow-auto scrollbar-hidden h-full w-full md:h-[692px] md:w-[320px] px-4 md:px-2 py-4 mb-4 space-y-2  md:border-gray-900 md:border-8 md:rounded-3xl shadow-sm flex flex-col items-center text-center font-${pageData.profile_font}`}
     >
-      <div className=" text-black flex flex-col gap-2 items-center">
+      <div className="mt-10 md:mt-0 text-black flex flex-col gap-2 items-center">
         {pageData.profile_photo_url && (
           <div className="">
             <Image
               src={pageData.profile_photo_url}
               width={50}
-              height={50}
+              height={50} 
               style={imageStyle}
               alt="link picture"
-              className=""
+              className="w-24 h-24 md:w-12 md:h-12"
             />
           </div>
         )}
 
         <div
           style={{ color: pageData.profile_font_color }}
-          className="text-sm font-bold"
+          className="text-xl md:text-sm font-bold"
         >
           {pageData.name ? pageData.name : `@${pageData.username}`}
         </div>
-        <div style={{ color: pageData.profile_font_color }} className="text-xs">
+        <div style={{ color: pageData.profile_font_color }} className="md:text-xs">
           {pageData.bio}
         </div>
       </div>
       {pageData.social_media_icons_location === "top" && (
         <div className="">
-          <ul className="flex gap-2 text-black">
+          <ul className="flex gap-4 md:gap-2 text-black md:mt-0 md:mb-0 mt-2 mb-2">
             {pageData.social_media_profiles &&
               pageData.social_media_profiles.map((profile) => (
                 <li key={profile.id}>
@@ -127,7 +127,7 @@ export default function PhonePreview({ pageData }: PhonePreviewProps) {
           </ul>
         </div>
       )}
-      <div className="flex flex-col gap-4 w-full">
+      <div className="flex flex-col gap-6 md:gap-4 w-full">
         {pageData.links &&
           pageData.links.map((link) => (
             <a
@@ -135,7 +135,7 @@ export default function PhonePreview({ pageData }: PhonePreviewProps) {
               key={link.id}
               href={link.link}
               style={getLinkBubbleColorStyle()}
-              className={`${getLinkBubbleStyle()} relative flex items-center justify-center text-xs px-3 py-3 h-12`}
+              className={`${getLinkBubbleStyle()} relative flex items-center justify-center md:px-3 md:py-3 md:h-12 px-4 py-4 h-16 transform hover:scale-105 md:hover:scale-101 transition duration-150`}
             >
               {link.link_photo_url && (
                 <Image
@@ -144,16 +144,16 @@ export default function PhonePreview({ pageData }: PhonePreviewProps) {
                   height={36}
                   style={imageStyle}
                   alt={link.title}
-                  className="absolute left-1.5 top-1.5"
+                  className="absolute left-2 md:left-1.5 md:top-1.5 w-11 h-11 md:w-9 md:h-9"
                 />
               )}
-              <p className="text-center text-xs">{link.title}</p>
+              <p className="text-center text-sm md:text-xs">{link.title}</p>
             </a>
           ))}
       </div>
       {pageData.social_media_icons_location === "bottom" && (
         <div className="">
-          <ul className="flex gap-2 text-black">
+          <ul className="flex gap-4 md:gap-2 text-black">
             {pageData.social_media_profiles &&
               pageData.social_media_profiles.map((profile) => (
                 <li key={profile.id}>
