676fd6bfd9129be5750fbdffd84be8d5a7877326
Cathy Do <catherinexdo@gmail.com>
2025-02-25 12:44:41 -0800

    update landing page assets, add avatar carousel, change styling

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,6 +10,15 @@ export default function Home() {
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
   const [authType, setAuthType] = useState("");
 
+  const avatars = [
+    { src: "https://linknbio-resources.s3.amazonaws.com/1b99eb8a-71c2-4a50-8448-0a34e2dc1a29.png", name: "Maya James", link: "/share/maya_james", desc: "Fashion Influencer"},
+    { src: "https://linknbio-resources.s3.amazonaws.com/bcc1463c-cf4d-47f2-a384-5846268fd8e7.png", name: "The Golden Crust", link: "/share/thegoldencrust", desc: "Laminated Baked Goods" },
+    { src: "https://linknbio-resources.s3.amazonaws.com/177d79f6-fcb2-401a-a1aa-5199519d1c1b.png", name: "Fur Babies Rescue", link: "/share/fur_babies_rescue" , desc: "Non-profit"},
+    { src: "https://linknbio-resources.s3.amazonaws.com/f16d1b9d-3779-4fcd-8dac-3af39779fd64.png", name: "Pure Radiance Co", link: "/share/pureradianceco" , desc: "Natural Shampoo Bars"},
+    { src: "https://linknbio-resources.s3.amazonaws.com/8e9379c6-3585-4ea9-a4a1-8eed465b89fa.png", name: "Logan Steele", link: "/share/logansteele" , desc: "Musician"},
+    { src: "https://linknbio-resources.s3.amazonaws.com/173b76f7-d99f-40fa-b313-63042f4718d2.png", name: "Tessa's Table", link: "/share/tessastable", desc: "Food Blog" },
+  ]
+
   function handleAuthButtonClick(type: string) {
     setAuthType(type);
     setIsLoginModalOpen(true);
@@ -28,15 +37,15 @@ export default function Home() {
             <div className="flex items-center lg:order-2 gap-2">
               <button
                 onClick={() => handleAuthButtonClick("login")}
-                className="border border-black text-black bg-white hover:bg-black hover:text-white font-bold py-2 px-4 rounded-lg "
+                className=" bg-stone-200 hover:bg-stone-300  text-black py-2 px-6 text-sm font-semibold rounded-lg "
               >
-                Login
+                Log in
               </button>
               <button
                 onClick={() => handleAuthButtonClick("signup")}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded-full"
+                className="bg-custom-blue-500 hover:bg-custom-blue-200 text-black py-2 px-6 font-semibold text-sm rounded-lg"
               >
-                Signup
+                Sign up
               </button>
             </div>
           </div>
@@ -53,7 +62,7 @@ export default function Home() {
               place. Create a page that matches your style and never worry about
               changing your bio link again.            </h3>
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 w-max md:w-full rounded-full mt-8"
+              className="bg-custom-blue-500 hover:bg-custom-blue-200 text-black font-bold py-3 px-8  w-max md:w-fit rounded-lg mt-8"
               onClick={() => handleAuthButtonClick("signup")}
             >
               Get Started
@@ -69,86 +78,110 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="bg-indigo-200 py-20 md:py-40 px-4 lg:px-24 w-full flex flex-col md:flex-row-reverse items-center gap-12">
+        <div className="py-32 bg-stone-800 text-center w-full text-white flex flex-col items-center gap-20">
+          <h2 className="text-3xl font-bold italic">Check out these <span className="border-b-4 border-custom-blue-500">Linknbios</span> for inspiration</h2>
+          <div className="flex gap-16 overflow-x-auto flex-nowrap w-full px-8 pb-10">
+            {avatars.map((avatar, index) => (
+                <a
+                  target="_blank"
+                  key={index} 
+                  href={avatar.link}
+                  rel="noopener noreferrer" 
+                  className="flex flex-col items-center w-48 ">
+                  <div className="w-48 h-48 p-4 hover:scale-105 transition-transform duration-300">
+                    <Image
+                      src={avatar.src}
+                      alt={avatar.name}
+                      width={192}
+                      height={192}
+                      className="rounded-full"
+                    />
+                  </div>
+                  <span className="font-bold tracking-wider text-center mt-4">{avatar.name}</span>
+                  <span className="text-sm mt-2">{avatar.desc}</span>
+                </a>
+            ))}
+          </div>
+        </div>
+        <div className="bg-stone-100 py-20 md:py-40 px-4 lg:px-24 w-full flex flex-col items-center text-center  gap-12">
           <div className="flex flex-col flex-1">
-            <h1 className="text-4xl md:text-5xl font-extrabold text-blue-900 tracking-tight">
+            <h1 className="text-4xl md:text-5xl font-extrabold text-black tracking-tight">
               Your page up and running in minutes, customized to your personal brand
             </h1>
-            <h3 className="text-m md:text-xl mt-4 font-semibold text-blue-900">
+            <h3 className="text-m md:text-xl mt-4 font-semibold text-black">
               No need to download anything. Easily add all your links and personalize the page to your style.
             </h3>
             <div>
-              <button
-                className="bg-blue-900 hover:bg-blue-950 text-white font-bold py-4 px-8 rounded-full mt-6"
-                onClick={() => handleAuthButtonClick("signup")}
-              >
-                Get Started
-              </button>
+            <button
+              className="bg-custom-blue-500 hover:bg-custom-blue-200 text-black font-bold py-3 px-8 w-max md:w-fit rounded-lg mt-8"
+              onClick={() => handleAuthButtonClick("signup")}
+            >
+              Get Started
+            </button>
               </div>
           </div>
           <div className="flex-1">
-            <Image
-              src="/hero-graphic.png"
-              width={1178}
-              height={1276}
-              alt="hero-image"
-              className="w-full h-auto"
-            />
+             <video autoPlay loop muted playsInline className="border-4 border-black rounded-lg max-w-3xl w-full">
+              <source src="/AppearanceRecording.mp4" type="video/mp4" />
+             </video>
           </div>
         </div>
-        <div className="bg-fuchsia-100 py-20 md:py-40 px-4 lg:px-24 w-full flex flex-col md:flex-row items-center gap-12">
-          <div className="flex flex-col flex-1">
-            <h1 className="text-4xl md:text-5xl font-extrabold text-rose-900 tracking-tight">
+        <div className=" bg-white py-24 px-4 md:px-12 w-full flex flex-col md:flex-row items-center gap-12">
+          <div className="flex flex-col flex-1 md:p-8 rounded-2xl">
+            <h1 className="text-4xl md:text-5xl font-extrabold text-black tracking-tight">
             Share your Linknbio on your Instagram, Youtube, TikTok, Twitter and other platforms
             </h1>
-            <h3 className="text-m md:text-xl mt-4 font-semibold text-rose-900">
+            <h3 className="text-m md:text-xl mt-4 font-semibold text-black">
             Add your Linknbio URL wherever your audience is. Direct all your following to one location.
 
             </h3>
             <div>
               <button
-                className="bg-rose-900 hover:bg-rose-950 text-white font-bold py-4 px-8 rounded-full mt-6"
+                className="bg-custom-blue-500 hover:bg-custom-blue-200 text-black font-bold py-3 px-8 w-max md:w-fit rounded-lg mt-8"
                 onClick={() => handleAuthButtonClick("signup")}
               >
                 Get Started
               </button>
-              </div>
+            </div>
           </div>
           <div className="flex-1">
             <Image
-              src="/SocialsLanding.svg"
+              src="/SocialsLanding4.svg"
               width={938}
               height={599}
               alt="hero-image"
-              className="w-full h-auto"
+              className="w-full max-w-[100%] h-auto object-contain"
             />
           </div>
         </div>
-        <div className="bg-rose-300 py-20 md:py-40 px-4 lg:px-24 w-full flex flex-col md:flex-row-reverse items-center gap-12">
-          <div className="flex flex-col flex-1">
-            <h1 className="text-4xl md:text-5xl font-extrabold text-indigo-700 tracking-tight">
+{/*         <div className="bg-custom-red-50 text-custom-red-600 text-3xl font-semibold text-center flex w-full justify-center py-36 px-24">
+          "Ever since using Linknbio we've seen a 45% increase in our website traffic"
+        </div> */}
+        <div className=" bg-stone-100 py-24 px-4 md:px-12 w-full flex flex-col md:flex-row-reverse items-center gap-12">
+          <div className="flex flex-col flex-1 md:p-8 rounded-2xl">
+            <h1 className="text-4xl md:text-5xl font-extrabold text-black tracking-tight">
               Analyze the performance of your Linknbio
 
             </h1>
-            <h3 className="text-m md:text-xl mt-4 text-indigo-700 font-semibold">
+            <h3 className="text-m md:text-xl mt-4 text-black font-semibold">
               See what&apos;s working and what&apos;s not. Track your page performance over time and keep your audience engaged.
             </h3>
             <div>
               <button
-                className="bg-indigo-700 hover:bg-indigo-800 text-white font-bold py-4 px-8 rounded-full mt-6"
+                className="bg-custom-blue-500 hover:bg-custom-blue-200 text-black font-bold py-3 px-8 w-max md:w-fit rounded-lg mt-8"
                 onClick={() => handleAuthButtonClick("signup")}
               >
                 Get Started
               </button>
               </div>
           </div>
-          <div className="flex-1 px-8">
+          <div className="flex-1 ">
             <Image
-              src="/AnalyticsLanding.svg"
+              src="/AnalyticsLanding3.svg"
               width={572}
               height={497}
               alt="hero-image"
-              className="w-full h-auto"
+              className="w-full h-auto max-w-[90%] object-contain"
             />
           </div>
         </div>
